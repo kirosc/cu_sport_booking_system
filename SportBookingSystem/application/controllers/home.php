f3f3beab24ca220cc70615727535ef67d138c2e1
kennydc822 <kennydc822@users.noreply.github.com>
2019-03-21 14:32:10 +0800

    basic config and test git

---
+++ SportBookingSystem/application/controllers/home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ */
+class Home extends CI_Controller
+{
+
+  public function index()
+  {
+    // code...
+    $this->load->view('index');
+  }
+}
+
+?>
---
+++ SportBookingSystem/application/views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+<p>This is the Home page</p>
+
+</body>
+</html>
