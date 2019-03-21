a035ae4df84f6d5ebd1a205605fd7c49f86ffeb2
Bobby Chan <extremefever926@gmail.com>
2019-03-21 23:18:57 +0800

    Modify Model Structure and Add Attribute
    Add User Model as SuperClass

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -2,9 +2,13 @@
 /**
  *
  */
-class Student_model extends CI_Model
+class Student_model extends User_model
 {
-
+    //Attribute
+    public $self_intro;     //Is this necessary?
+    public $phone_number;   //Apply to all? (To user class)
+    public $interest;
+    public $birthday;       //Apply to all? (To user class)
 }
 
 ?>
