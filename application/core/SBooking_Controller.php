54a55d0f317a6b7dd249cf28cbd7df5958faebab
kennydc822 <kennydc822@users.noreply.github.com>
2019-04-26 02:03:01 +0800

    Add comment on base controller

--- application/core/SBooking_Controller.php
+++ application/core/SBooking_Controller.php
@@ -1,7 +1,6 @@
 <?php
-/**
- *
- */
+
+//based controller of all controllers in controllers folder
 class SBooking_Controller extends CI_Controller
 {
   private $page_url, $image_url, $css_url, $js_url;
@@ -11,12 +10,13 @@ class SBooking_Controller extends CI_Controller
   {
     parent::__construct();
     date_default_timezone_set("Asia/Hong_Kong");
-    
+
     $this->page_url = base_url().'index.php/';
     $this->image_url = base_url().'images/';
     $this->css_url = base_url().'styles/';
     $this->js_url = base_url().'scripts/';
 
+    //store all the data we need that need to show on HTML header tag
     $this->header_data = array(
       'title' => '',
       'nav' => '',
@@ -26,6 +26,7 @@ class SBooking_Controller extends CI_Controller
       'js_file' => array()
     );
 
+    //base CSS file
     $this->loadCSS('normalize.css');
     $this->loadCSS('style.css');
     $this->loadCSS('libraries/bootstrap.min.css');
@@ -34,26 +35,31 @@ class SBooking_Controller extends CI_Controller
     $this->loadCSS('header.css');
     $this->loadCSS('footer.css');
 
+    //base JS file
     $this->loadJS('libraries/jquery-3.3.1.min.js');
     $this->loadJS('libraries/popper.min.js');
     $this->loadJS('libraries/bootstrap.min.js');
   }
 
+  //set HTML title
   protected function setTitle($title)
   {
     $this->header_data['title'] = $title;
   }
 
+  //set HTML page type
   protected function setNav($nav)
   {
     $this->header_data['nav'] = $nav;
   }
 
+  //return header data to controller
   protected function getHeaderData()
   {
     return $this->header_data;
   }
 
+  //push CSS file into header_data array
   protected function loadCSS($path)
   {
     array_push(
@@ -62,6 +68,7 @@ class SBooking_Controller extends CI_Controller
     );
   }
 
+  //push CDNCSS file into header_data array
   protected function loadCDNCSS($path)
   {
     array_push(
@@ -69,6 +76,7 @@ class SBooking_Controller extends CI_Controller
     );
   }
 
+  //push JS file into header_data array
   protected function loadJS($path)
   {
     array_push(
@@ -77,6 +85,7 @@ class SBooking_Controller extends CI_Controller
     );
   }
 
+  //push CDNJS file into header_data array
   protected function loadCDNJS($path)
   {
     array_push(
